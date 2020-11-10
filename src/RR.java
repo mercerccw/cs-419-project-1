9ff6dd1f37e835cac5b8144e22802c24b1d060f5
Clayton Mercer <mercerccw@gmail.com>
2020-11-09 19:11:09 -0500

    Fix SJF Bug

--- src/RR.java
+++ src/RR.java
@@ -1,15 +1,16 @@
 /**
- *
  * This algorithm will run tasks according to round-robin scheduling.
- *
+ * <p>
  * Add your implementation inside the RR class below.
- *
  */
- 
+
 import java.util.*;
 
-public class RR implements Algorithm
-{
+public class RR implements Algorithm {
+    private List<Task> queue;
+    private Task currentTask;
+    private int tasksRun;
+    private List<Task> runningQueue;
 
     @Override
     public void schedule() {
--- src/SJF.java
+++ src/SJF.java
@@ -10,38 +10,52 @@ public class SJF implements Algorithm {
     private List<Task> queue;
     private Task currentTask;
     private int tasksRun;
-    private List<Task> waitingQueue;
+    private List<Task> waitingQueue = new ArrayList<Task>();
+
 
     public SJF(List<Task> queue) {
         this.queue = queue;
-
         tasksRun = queue.size();
     }
 
     @Override
     public void schedule() {
         System.out.println("Shortest Job First Scheduling\n");
-        while (!queue.isEmpty()) {
+        while (!queue.isEmpty() || !waitingQueue.isEmpty()) {
             currentTask = pickNextTask();
-
             CPU.run(currentTask, currentTask.getBurst());
-            System.out.println("Task " + currentTask.getName() + " finished at time " + CPU.getCurrentTime());
-
-            // remove the task
-            queue.remove(currentTask);
+            if (!waitingQueue.isEmpty()) {
+                System.out.println("Task " + currentTask.getName() + " finished at time " + CPU.getCurrentTime());
+                waitingQueue.remove(currentTask);
+                queue.remove(currentTask);
+            } else {
+                System.out.println("Task " + currentTask.getName() + " finished at time " + CPU.getCurrentTime());
+                queue.remove(currentTask);
+            }
+            for (Task task : queue) {
+                if (task.getArrivalTime() < CPU.getCurrentTime()) {
+                    waitingQueue.add(task);
+                } else {
+                    break;
+                }
+            }
         }
     }
 
+
     @Override
     public Task pickNextTask() {
+        Comparator<Task> compareBurstTime = Comparator.comparingInt(Task::getBurst);
         if (tasksRun > queue.size()) {
-            Task smallest_task = queue.get(0);
-            for (Task task: queue) {
-                if(task.getBurst() < smallest_task.getBurst()){
-                    smallest_task = task;
+            if (!waitingQueue.isEmpty()) {
+                for (Task task: waitingQueue) {
+                    queue.remove(task);
                 }
+                waitingQueue.sort(compareBurstTime);
+                return waitingQueue.get(0);
+            } else {
+                return queue.get(0);
             }
-            return queue.get(queue.indexOf(smallest_task));
         } else {
             return queue.get(0);
         }
